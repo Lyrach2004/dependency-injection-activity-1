1686d4762f2c0ec4ecd5efa9dc89ae147527dac8
Lyrach2004 <charlyzoungrana2004@gmail.com>
2026-02-19 19:27:49 +0000

    implémentation complète de l’injection des dépendances (constructeur, setter, field)

--- partie-2-mini-framework/mini-ioc-framework/src/main/java/net/lyrach/core/SimpleApplicationContext.java
+++ partie-2-mini-framework/mini-ioc-framework/src/main/java/net/lyrach/core/SimpleApplicationContext.java
@@ -11,6 +11,43 @@ public class SimpleApplicationContext implements BeanFactory {
         this.beans.put(name,bean);
     }
 
+    public Object createBean(String className){
+        try{
+            Class<?> clazz = Class.forName(className);
+            return clazz.getDeclaredConstructor().newInstance();
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Object createBeanWithConstructor(String className, Class<?>[] paramTypes, Object[] args) {
+        try {
+            Class<?> clazz = Class.forName(className);
+            return clazz.getConstructor(paramTypes).newInstance(args);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void injectSetter(Object bean, String methodName, Object dependency) {
+        try {
+            var method = bean.getClass().getMethod(methodName, dependency.getClass());
+            method.invoke(bean, dependency);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void injectField(Object bean, String fieldName, Object dependency) {
+        try {
+            var field = bean.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            field.set(bean, dependency);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public Object getBean(String name) {
         return this.beans.get(name);
